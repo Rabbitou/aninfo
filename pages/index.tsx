92fe24b3e98e3a720a06cabea09e01f63c54a7fc
Rabbitou <48840164+Rabbitou@users.noreply.github.com>
2022-10-17 19:23:39 +0100

    add gap index

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             <h2 className="p-4">Season Anime</h2>
             <hr className="mx-4" />
           </div>
-          <div className="animeseasonlist m-4 grid xs:grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-2">
+          <div className="animeseasonlist m-4 grid xs:grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
             <Animedata />
             <Animedata />
             <Animedata />
